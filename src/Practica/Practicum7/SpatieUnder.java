925fddab2191153305225d2078822462270edd8f
Milan Dol <milandol321@gmail.com>
2020-03-16 20:59:35 +0100

    Practicum 7 af + eigen toevoeging om het te begrijpen.

--- src/Practica/Practicum7/SpatieUnder.java
+++ src/Practica/Practicum7/SpatieUnder.java
@@ -1,17 +1,9 @@
 package Practica.Practicum7;
 
 public class SpatieUnder implements OpmaakProces{ // Zelf toegevoegd om het te begrijpen.
-    private String oud;
-    private String nieuw;
-
-    public SpatieUnder(String oud, String nieuw){
-        this.oud = oud;
-        this.nieuw = nieuw;
-    }
-
 
     @Override
     public String maakOp(String input) {
-        return input.replace(oud, nieuw);
+        return input.replace(" ", "_");
     }
 }

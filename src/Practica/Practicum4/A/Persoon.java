b6b9cd9f32c49de29e597400835ce52a70d05f78
Milan Dol <milandol321@gmail.com>
2020-03-20 23:07:40 +0100

    Zodat alles niet rood wordt.

--- src/Practica/Practicum4/A/Persoon.java
+++ src/Practica/Practicum4/A/Persoon.java
@@ -4,12 +4,9 @@ public class Persoon {
     private String naam;
     private int leeftijd;
 
-
-
     public Persoon(String nm, int lft) {
         this.naam = nm;
         this.leeftijd = lft;
-
     }
 
     @Override

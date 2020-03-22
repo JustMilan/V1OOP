fafc56708b0581b6b0b7623639a7362a4aa252fa
Milan Dol <milandol321@gmail.com>
2020-03-22 18:52:41 +0100

    Begin practicum 11 toegevoegd.

---
+++ src/Practica/Practicum11/Omvormer.java
@@ -0,0 +1,10 @@
+package Practica.Practicum11;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Omvormer {
+    public static void main(String[] args) throws IOException {
+        Path path = Path.of("artikelenDLR.txt");
+    }
+}
